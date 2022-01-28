18669e1c51fc1ae3abf0d149cd838e32f9693707
Syed Ismail <Syed Ismail@Syed-Benazir>
2022-01-28 07:47:06 +0530

    My First Commit

---
+++ SyedJavaTraining/src/javatraining/day1calculator.java
@@ -0,0 +1,14 @@
+package javatraining;
+
+public class day1calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=5;
+		int b=6;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
+}
